dc7504c746268258f8ea364b53ba8926a5986e91
jon-a-nygaard <jon.arild@highsoft.com>
2017-08-07 21:53:13 +0200

    Added main entry point.

--- test/test.js
+++ test/test.js
@@ -3,3 +3,12 @@ import './build.js';
 import './dependencies.js';
 import './process.js';
 import './utilities.js';
+import { expect } from 'chai';
+import main from '../index.js';
+
+describe('index.js', () => {
+    it('has a primary entry point', () => {
+        expect(main).to.be.a('function');
+        expect(main.name).to.equal('build');
+    });
+});
